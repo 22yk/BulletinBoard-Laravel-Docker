0f789a95c43e914ffeaef69c43546bd9032abeed
yuki-fujisaki <fuji.blue.2417@gmail.com>
2022-03-31 03:56:13 +0900

    CRUD

--- source/routes/web.php
+++ source/routes/web.php
@@ -20,3 +20,8 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::resource('posts', 'PostController', ['only' => ['index','show', 'create', 'store']]);
+Route::get('posts/edit/{id}', 'PostController@edit');
+Route::post('posts/edit', 'PostController@update');
+Route::post('posts/delete/{id}', 'PostController@destroy');
